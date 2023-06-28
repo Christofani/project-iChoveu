1752a6ca8760e23cdef36d19bea5a3a458ead0b2
Rodrigo Christofani <christofanir@gmail.com>
2023-06-27 22:03:07 -0300

    Resolução requisito 2

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -13,6 +13,16 @@ export async function searchCities(term) {
   }
 }
 
-export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
-};
+export async function getWeatherByCity(cityURL) {
+  try {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${cityURL}`);
+    const data = await response.json();
+    return data.map(({ current }) => ({
+      temp: current.temp_c,
+      condition: current.condition.text,
+      icon: current.condition.icon,
+    }));
+  } catch (error) {
+    return error;
+  }
+}
